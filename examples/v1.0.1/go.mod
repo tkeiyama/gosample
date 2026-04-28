afbf8285fddfecedec85156151a400623d7e1763
tkeiyama <thomas18thx@gmail.com>
2026-04-28 06:20:13 +0000

    v1.0.1

---
+++ examples/v1.0.1/go.mod
@@ -0,0 +1,5 @@
+module v1.0.1
+
+go 1.26.2
+
+require github.com/tkeiyama/gosample v1.0.1 // indirect
---
+++ examples/v1.0.1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tkeiyama/gosample"
+)
+
+func main() {
+	fmt.Println(gosample.Greet("World"))
+}
